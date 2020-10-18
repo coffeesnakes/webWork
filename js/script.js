849adde93d8b1833ade93922e030130ecd018771
Jacob Berg <coffeesnakes@gmail.com>
2020-10-18 15:53:29 -0700

    button functionality and display of content working

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-//setup
+
+function sayHowdy () {
+  const name = document.getElementById("name").value;
+
+  const message = "Howdy there " + name + " welcome to WebWork!";
+
+ document.getElementById("content").textContent = message
+};
