328fa62cc8b080e2da7052dad53a5837ac7bdaaa
Jacob Berg <coffeesnakes@gmail.com>
2020-10-19 21:32:21 -0700

    hmm

--- js/script.js
+++ js/script.js
@@ -2,7 +2,14 @@
 function sayHowdy () {
   const name = document.getElementById("name").value;
 
-  const message = "Howdy there " + name + " welcome to WebWork!";
-
+  const message = "howdy there " + name + " welcome to WebWork!";
+// reaches out to the content div inside index.html and displays the message with variable input from the button
  document.getElementById("content").textContent = message
-};
+
+if (name === "Jake") {
+  const x = 'wee'
+const title = document.querySelector("#title").textContent;
+title += x;
+  document.querySelector("#title").textContent = title;
+}
+}
